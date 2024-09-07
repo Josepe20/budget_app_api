6fe15d45686924215ce24aad8a67150ea8c99737
Jose Chay <chepechay16@gmail.com>
2024-09-07 12:30:03 -0600

    [UPDATE][routers path updated for income and expenses]

--- app/router/budget/budget_router.py
+++ app/router/budget/budget_router.py
@@ -4,12 +4,9 @@ from app.dependencies import get_db_session
 from app.functions.api_response import standard_response
 from app.schemas.budget.budget_schema import BudgetCreate, BudgetResponse
 from app.views.budget import budget_view
-from app.router.budget.routes import incomes_router, expenses_router
 
 
 router = APIRouter()
-router.include_router(incomes_router.router, prefix="/incomes", tags=["incomes"])
-router.include_router(expenses_router.router, prefix="/expenses", tags=["expenses"])
 
 
 @router.get("/")
