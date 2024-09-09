b076ba5a5c3b687e728c95adb14e4b64f41f18a2
Jose Chay <chepechay16@gmail.com>
2024-09-08 22:04:08 -0600

    [FIX][convert sqlalchemy objet to pydantic objects, needs a fix for list]

--- app/functions/api_response.py
+++ app/functions/api_response.py
@@ -11,29 +11,37 @@ def standard_response(status: int, message: str, data: Any = None) -> Dict:
 
 
 from fastapi.responses import JSONResponse
-from typing import TypeVar, Any
+from typing import TypeVar, Any, Type
 from pydantic import BaseModel
+from sqlalchemy.orm import DeclarativeMeta
 from datetime import datetime
 
 T = TypeVar("T", bound=BaseModel)
 
-def standard_response(status: int, message: str, data: T = None) -> JSONResponse:
-    # Manually serialize datetime fields if present
+def standard_response(status: int, message: str, data: Any = None, pydantic_model: Type[T] = None) -> JSONResponse:
+    # Si data es una instancia de un modelo Pydantic
     if isinstance(data, BaseModel):
-        data_dict = data.model_dump()
-        for key, value in data_dict.items():
+        data = data.model_dump()
+
+    # Si data es una instancia de un modelo SQLAlchemy (ORM)
+    elif isinstance(type(data), DeclarativeMeta) and pydantic_model is not None:
+        # Convertimos el modelo SQLAlchemy a un modelo Pydantic usando el tipo provisto
+        data = pydantic_model.model_validate(data).model_dump()
+
+    # Convertimos los campos de tipo datetime a ISO format si están presentes
+    if isinstance(data, dict):
+        for key, value in data.items():
             if isinstance(value, datetime):
-                data_dict[key] = value.isoformat()  # Convert datetime to ISO format
-    else:
-        data_dict = data
+                data[key] = value.isoformat()
 
     response_content = {
         "status": status,
         "message": message,
-        "data": data_dict
+        "data": data
     }
 
     return JSONResponse(content=response_content, status_code=status)
+
 
 
 
--- app/router/budget/incomes_router.py
+++ app/router/budget/incomes_router.py
@@ -12,36 +12,36 @@ router = APIRouter()
 @router.post("/create", response_model=StandardResponse[IncomeResponse])
 def create_income(income: IncomeCreate, db: Session = Depends(get_db_session)):
     created_income = incomes_view.create_income(income, db)
-    return standard_response(status.HTTP_201_CREATED, "income created succesfully", created_income)   
+    return standard_response(status.HTTP_201_CREATED, "income created succesfully", created_income, pydantic_model=IncomeResponse)   
 
 
 @router.put("/update/{income_id}", response_model=StandardResponse[IncomeResponse])
 def update_income(income_id: int, income: IncomeCreate, db: Session = Depends(get_db_session)):
     updated_income = incomes_view.update_income(income_id, income.model_dump(), db)
-    return standard_response(status.HTTP_200_OK, "income updated successfully", updated_income)
+    return standard_response(status.HTTP_200_OK, "income updated successfully", updated_income, pydantic_model=IncomeResponse)
     
 
 @router.delete("/delete/{income_id}", response_model=StandardResponse[IncomeResponse])
 def delete_income(income_id: int, db: Session = Depends(get_db_session)):
     deleted_message = incomes_view.delete_income(income_id, db)
-    return standard_response(status.HTTP_200_OK, "income deleted succesfully", deleted_message)
+    return standard_response(status.HTTP_200_OK, "income deleted succesfully", deleted_message, pydantic_model=IncomeResponse)
     
 
 @router.get("/{income_id}", response_model=StandardResponse[IncomeResponse])
 def get_income_by_id(income_id: int, db: Session = Depends(get_db_session)):
     income_found = incomes_view.get_income_by_id(income_id, db)
-    return standard_response(status.HTTP_200_OK, "income found", income_found)
+    return standard_response(status.HTTP_200_OK, "income found", income_found, pydantic_model=IncomeResponse)
     
 
 @router.get("/user/{user_id}", response_model=StandardResponse[list[IncomeResponse]])
 def get_user_incomes(user_id: int, db: Session = Depends(get_db_session)):
     incomes_list = incomes_view.get_user_incomes(user_id, db)
-    return standard_response(status.HTTP_200_OK, "incomes found", incomes_list)
+    return standard_response(status.HTTP_200_OK, "incomes found", incomes_list, pydantic_model=IncomeResponse)
     
 
 @router.get("/user/{user_id}/active", response_model=StandardResponse[list[IncomeResponse]])
 def get_user_active_incomes(user_id: int, db: Session = Depends(get_db_session)):
     incomes_list = incomes_view.get_user_active_incomes(user_id, db)
-    return standard_response(status.HTTP_200_OK, "incomes found", incomes_list)
+    return standard_response(status.HTTP_200_OK, "incomes found", incomes_list, pydantic_model=IncomeResponse)
 
     
