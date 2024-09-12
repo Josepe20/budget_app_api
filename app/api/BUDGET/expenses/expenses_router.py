4eb632a4f71268373c47175864137a40b9600913
Jose Chay <chepechay16@gmail.com>
2024-09-11 23:51:24 -0600

    [ADD][expenses module finished, needs manual testing and unit test]

--- app/api/BUDGET/expenses/expenses_router.py
+++ app/api/BUDGET/expenses/expenses_router.py
@@ -1,3 +1,58 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from app.dependencies import get_db_session
+from app.common.functions.api_response import standard_response
+from app.api.BUDGET.expenses.expenses_schema import ExpenseCreate, ExpenseResponse
+from app.api.BUDGET.expenses import expenses_view
+from app.common.schemas.response_schema import StandardResponse
+
 
 router = APIRouter()
+
+
+@router.post("/create", response_model=StandardResponse[ExpenseResponse])
+def create_expense(expense: ExpenseCreate, db: Session = Depends(get_db_session)):
+    created_expense = expenses_view.create_expense(expense, db)
+    return standard_response(status.HTTP_201_CREATED, "expense created succesfully", created_expense, pydantic_model=ExpenseResponse)   
+
+
+@router.put("/update/{expense_id}", response_model=StandardResponse[ExpenseResponse])
+def update_expense(expense_id: int, expense: ExpenseCreate, db: Session = Depends(get_db_session)):
+    updated_expense = expenses_view.update_expense(expense_id, expense.model_dump(), db)
+    return standard_response(status.HTTP_200_OK, "expense updated successfully", updated_expense, pydantic_model=ExpenseResponse)  
+
+
+@router.delete("/delete/{expense_id}", response_model=StandardResponse[ExpenseResponse])
+def delete_expense(expense_id: int, db: Session = Depends(get_db_session)):
+    deleted_expense = expenses_view.delete_expense(expense_id, db)
+    return standard_response(status.HTTP_200_OK, "expense deleted succesfully", deleted_expense, pydantic_model=ExpenseResponse)
+
+
+@router.get("/{expense_id}", response_model=StandardResponse[ExpenseResponse])
+def get_expense_by_id(expense_id: int, db: Session = Depends(get_db_session)):
+    expense_found = expenses_view.get_expense_by_id(expense_id, db)
+    return standard_response(status.HTTP_200_OK, "expense found", expense_found, pydantic_model=ExpenseResponse)
+    
+
+@router.get("/user/{user_id}", response_model=StandardResponse[list[ExpenseResponse]])
+def get_user_expenses(user_id: int, db: Session = Depends(get_db_session)):
+    expenses_list = expenses_view.get_user_expenses(user_id, db)
+    return standard_response(status.HTTP_200_OK, "expenses found", expenses_list, pydantic_model=ExpenseResponse)
+    
+
+@router.get("/user/{user_id}/active", response_model=StandardResponse[list[ExpenseResponse]])
+def get_user_active_expenses(user_id: int, db: Session = Depends(get_db_session)):
+    expenses_list = expenses_view.get_user_active_expenses(user_id, db)
+    return standard_response(status.HTTP_200_OK, "expenses found", expenses_list, pydantic_model=ExpenseResponse)
+
+
+@router.get("/user/{user_id}/category/{category_id}", response_model=StandardResponse[list[ExpenseResponse]])
+def get_user_expenses_by_category(user_id: int, category_id: int, db: Session = Depends(get_db_session)):
+    expenses_list = expenses_view.get_user_expenses_by_category(user_id, category_id, db)
+    return standard_response(status.HTTP_200_OK, "expenses found", expenses_list, pydantic_model=ExpenseResponse)
+
+
+@router.get("/user/{user_id}/category/{category_id}/active", response_model=StandardResponse[list[ExpenseResponse]])
+def get_user_active_expenses_by_category(user_id: int, category_id: int, db: Session = Depends(get_db_session)):
+    expenses_list = expenses_view.get_user_expenses_by_category(user_id, category_id, db)
+    return standard_response(status.HTTP_200_OK, "expenses found", expenses_list, pydantic_model=ExpenseResponse)
---
+++ app/api/BUDGET/expenses/expenses_view.py
@@ -0,0 +1,130 @@
+from sqlalchemy.orm import Session
+from datetime import datetime, timezone
+from fastapi import HTTPException, status
+from app.api.BUDGET.expenses.expenses_model import Expenses
+from app.api.BUDGET.expenses.expenses_schema import ExpenseCreate
+from app.api.BUDGET.expenses.expenses_repository import ExpenseRepository
+from app.common.functions.validate_active_month import validate_active_month
+from app.api.BUDGET.budgets.budget_view import update_budget_totals
+
+
+def create_expense(expense_data: ExpenseCreate, db: Session):
+    expense_repository = ExpenseRepository(db)
+    new_expense = Expenses(
+        budget_id=expense_data.budget_id,
+        category_expense_id=expense_data.category_expense_id,
+        income_name=expense_data.income_name,
+        amount=expense_data.amount, 
+        created_at=datetime.now(timezone.utc)
+    )
+
+    validate_active_month(new_expense.created_at)
+
+    created_expense = expense_repository.create_expense(new_expense)
+
+    # update the total budget expense
+    update_budget_totals(
+        budget_id=expense_data.budget_id, 
+        type="income", 
+        operation="sum", 
+        amount=new_expense.amount, 
+        db=db
+    )
+
+    # Refresh the object state
+    db.refresh(created_expense)
+    return created_expense
+
+
+def update_expense(expense_id: int, expense_data: dict, db: Session):
+    expense_repository = ExpenseRepository(db)
+
+    expense_to_update = expense_repository.get_expense_by_id(expense_id)
+    if not expense_to_update:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found")
+    
+    validate_active_month(expense_to_update.created_at)
+
+    # Calculate the difference between the old and new value
+    difference = expense_data['amount'] - expense_to_update.amount
+
+    updated_expense = expense_repository.update_expense(expense_id, expense_data)
+
+    # update the total budget expense
+    update_budget_totals(
+        budget_id=updated_expense.budget_id,
+        type="income",
+        operation="sum" if difference > 0 else "sub",
+        amount=abs(difference),
+        db=db
+    )
+
+    # Refresh the object state
+    db.refresh(updated_expense)
+    return updated_expense
+
+
+def delete_expense(expense_id: int, db: Session):
+    expense_repository = ExpenseRepository(db)
+
+    expense_to_delete = expense_repository.get_expense_by_id(expense_id)
+    if not expense_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found")
+    
+    validate_active_month(expense_to_delete.created_at)
+
+    # update the total budget expense
+    update_budget_totals(
+        budget_id=expense_to_delete.budget_id, 
+        type="income", 
+        operation="sub", 
+        amount=expense_to_delete.amount, 
+        db=db
+    )
+
+    deleted_expense = expense_repository.delete_income(expense_id)
+    return deleted_expense
+
+
+def get_expense_by_id(expense_id: int, db: Session):
+    expense_repository = ExpenseRepository(db)
+    expense = expense_repository.get_expense_by_id(expense_id)
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found")
+    return expense
+
+
+def get_user_expenses(user_id: int, db: Session):
+    expense_repository = ExpenseRepository(db)
+    expenses = expense_repository.get_user_expenses(user_id)
+    if not expenses:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No expenses found")
+    return expenses
+
+
+def get_user_active_expenses(user_id: int, db: Session):
+    expense_repository = ExpenseRepository(db)
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    expenses = expense_repository.get_user_active_expenses(user_id, current_month, current_year)
+    if not expenses:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No active expenses found")
+    return expenses
+
+
+def get_user_expenses_by_category(user_id: int, category_id: int, db: Session):
+    expense_repository = ExpenseRepository(db)
+    expenses = expense_repository.get_user_expenses_by_category(user_id, category_id)
+    if not expenses:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No expenses found")
+    return expenses
+
+
+def get_user_active_expenses_by_category(user_id: int, category_id: int, db: Session):
+    expense_repository = ExpenseRepository(db)
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    expenses = expense_repository.get_user_active_expenses_by_category(user_id, category_id, current_month, current_year)
+    if not expenses:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No active expenses found")
+    return expenses
